fc94dd0c3f0369a250e541675564b7ec4c5906b8
Jake Miller <j@jjcm.org>
2019-09-22 11:35:26 +1000

    added cover example

--- components/soci-column.js
+++ components/soci-column.js
@@ -26,6 +26,10 @@ export default class SociColumn extends SociComponent {
         width: 100%;
         height: 100%;
         display: block;
+        scrollbar-width: none;
+      }
+      :host scroll-container::-webkit-scrollbar {
+        display: none;
       }
       :host content {
         display: block;
