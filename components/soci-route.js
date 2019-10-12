4aaba79fb19f05c2885670f9b14f97cc7cd207e8
Jake Miller <j@jjcm.org>
2019-10-12 19:05:42 +1100

    vw -> vh

--- components/soci-route.js
+++ components/soci-route.js
@@ -10,7 +10,7 @@ export default class SociRouter extends SociComponent {
       :host {
         display: none;
         width: 100%;
-        height: 100vw;
+        height: 100vh;
         overflow: auto;
         box-sizing: border-box;
         transition: opacity 0.1s ease-in-out;
