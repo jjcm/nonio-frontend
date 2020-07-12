c4451de5fb78decf96af7aac9f5f8986d4bf8b0c
jjcm <j@jjcm.org>
2020-07-12 17:57:15 +1000

    Now has confirm/deny buttons, now has hover affordance, now has checks to make sure the object is an image

--- components/soci-avatar-uploader.js
+++ components/soci-avatar-uploader.js
@@ -9,55 +9,25 @@ export default class SociFileDrop extends SociComponent {
   css(){
     return `
       :host {
-        min-height: 240px;
         display: inline-flex;
-        align-items: center;
-        justify-content: center;
         flex-direction: column;
-        //border: 2px dashed var(--n2);
-        box-sizing: border-box;
-        border-radius: 8px;
         margin-bottom: 12px;
-        position: relative;
-        //transition: border 0.2s ease;
-        overflow: hidden;
       }
 
-      :host([dragover]) {
-        //border: 2px dashed var(--g1);
+      :host([dragover]) #container {
+        border: 2px dashed var(--g1);
         transition: border 0.1s ease-out;
       }
 
-      :host:before {
-        content: '';
-        display: block;
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background: var(--g1);
-        opacity: 0;
-        transition: opacity 0.2s ease;
-        pointer-events: none;
-      }
-
-      :host([dragover]):before {
-        opacity: 0.1;
-        transition: opacity 0.1s ease-out;
-        z-index: -1;
-      }
-
-      :host([preview]) {
-        min-height: 0;
-        border-color: transparent;
-        transition: all 0.2s ease-in-out;
+      #container {
+        border-radius: 8px;
+        position: relative;
         overflow: hidden;
+        border: 2px dashed transparent;
+        transition: border 0.2s ease;
+        margin: -2px;
       }
-      :host([preview]):before {
-        opacity: 0;
-        transition: all 0.2s ease-in-out;
-      }
+
       img {
         max-width: 420px;
         max-height: 420px;
@@ -68,6 +38,8 @@ export default class SociFileDrop extends SociComponent {
       }
       #resizer {
         position: absolute;
+        opacity: 0;
+        transition: opacity 0.1s ease-in-out;
       }
       #drag {
         border: 2px dashed #fff;
@@ -165,30 +137,74 @@ export default class SociFileDrop extends SociComponent {
         transition: opacity 0.1s ease-in-out;
       }
 
-      :host([drop]) svg {
+      :host([cropping]) #resizer,
+      :host([cropping]) svg {
         opacity: 1;
       }
 
+      :host([cropping]) img {
+        pointer-events: none;
+      }
+
+
+     actions {
+        display: flex;
+        justify-content: flex-end;
+        padding: 4px 0 8px;
+      }
+
+      actions button {
+        border: 0;
+        border-radius: 3px;
+        background: var(--b2);
+        height: 20px;
+        color: #fff;
+        padding: 0 8px;
+        font-size: 12px;
+        margin-right: 4px;
+        cursor: pointer;
+      }
+
+      actions button.cancel {
+        background: var(--n1);
+        color: var(--n4);
+      }
+
+      actions button:hover {
+        filter: brightness(0.95);
+      }
+
+      actions button:focus,
+      actions button:active {
+        filter: brightness(0.9);
+        outline: 0;
+      }
     `
   }
 
   html(){ return `
-    <input id="file" type="file" accept="*"/>
-    <div id="resizer" @mousedown=_dragMouseDown>
-      <div class="resizer" id="nw"></div>
-      <div class="resizer" id="ne"></div>
-      <div class="resizer" id="sw"></div>
-      <div class="resizer" id="se" @mousedown=_seResizeDown></div>
-      <div id="drag"></div>
+    <input id="file" type="file" accept="image/*"/>
+    <div id="container">
+      <div id="resizer" @mousedown=_dragMouseDown>
+        <div class="resizer" id="nw"></div>
+        <div class="resizer" id="ne"></div>
+        <div class="resizer" id="sw"></div>
+        <div class="resizer" id="se"></div>
+        <div id="drag"></div>
+      </div>
+      <svg>
+        <mask id="mask">
+          <rect x="0" y="0" width="100%" height="100%" fill="white"/>
+          <circle cx="0" cy="0" r="10" fill="black"/>
+        </mask>
+        <rect x="0" y="0" width="100%" height="100%" fill="rgba(0,0,0,0.6)" mask="url(#mask)"/>
+      </svg>
+      <img src="/example-data/profile.jpg" @click=_selectFile />
     </div>
-    <svg>
-      <mask id="mask">
-        <rect x="0" y="0" width="100%" height="100%" fill="white"/>
-        <circle cx="0" cy="0" r="10" fill="black"/>
-      </mask>
-      <rect x="0" y="0" width="100%" height="100%" fill="rgba(0,0,0,0.6)" mask="url(#mask)"/>
-    </svg>
-    <img src="/example-data/profile.jpg"/>
+    <actions>
+      <button>submit</button>
+      <button class="cancel">cancel</button>
+    </actions>
   `}
 
   connectedCallback(){
@@ -197,27 +213,44 @@ export default class SociFileDrop extends SociComponent {
     )
 
     this.select("#file").addEventListener('change', this.upload.bind(this))
-    this.select("#file").addEventListener('change', ()=>{console.log('file changed')})
-    this.select("#file").addEventListener('input', ()=>{console.log('file input')})
+    this.select("#file").addEventListener('change', this._loadCropPreview.bind(this))
     this._resizer = this.select('#resizer')
   }
 
-  static get observedAttributes() {
-    return ['type']
+  _selectFile(e){
+    this.select('input').click()
   }
 
-  attributeChangedCallback(name, oldValue, newValue){
-    if(name == 'type') {
-      const TYPES = {
-        image: 'image/*',
-        video: 'video/*',
-        audio: 'audio/*',
-        html: 'zip,application/octet-stream,application/zip,application/x-zip,application/x-zip-compressed'
-      }
-      this.select('div').innerHTML = `drag ${newValue} here`
-      this.select('label').innerHTML = `select ${newValue}`
-      this.select('input').setAttribute('accept', TYPES[newValue])
-    }
+  _loadCropPreview(e){
+    let files = e?.target?.files[0]
+    if(files == null || files.type.indexOf("image/") != 0) return 0
+    this.toggleAttribute('cropping', true)
+    let preview = this.select('img')
+    let reader = new FileReader()
+    reader.addEventListener('load', e => {
+      preview.src = e.target.result
+    })
+    reader.addEventListener('loadend', ()=>{
+      this.width = preview.offsetWidth
+      this.height = preview.offsetHeight
+      this._cropSize = Math.min(this.width, this.height)
+      let resizer = this.select('#resizer')
+      resizer.style.width = this._cropSize + 'px'
+      resizer.style.height = this._cropSize + 'px'
+      this._positionX = (this.width - this._cropSize) / 2
+      this._positionY = (this.height - this._cropSize) / 2
+      resizer.style.left = this._positionX + 'px'
+      resizer.style.top = this._positionY + 'px'
+
+      this._mask = this.select('circle')
+      let radius = this._cropSize / 2
+      this._mask.setAttribute('r', radius)
+      this._mask.setAttribute('cx', radius + this._positionX)
+      this._mask.setAttribute('cy', radius + this._positionY)
+
+    })
+    reader.readAsDataURL(files)
+
   }
 
   _dragenter(e){
@@ -237,38 +270,12 @@ export default class SociFileDrop extends SociComponent {
     e.preventDefault()
     e.stopPropagation()
     this.toggleAttribute('dragover', false)
-    this.toggleAttribute('drop', true)
+    if(e.dataTransfer.files.length == 0) return 0
 
     let input = this.select('#file')
     input.files = e.dataTransfer.files
     let event = new Event('change')
     input.dispatchEvent(event)
-
-    let preview = this.select('img')
-    let reader = new FileReader()
-    reader.addEventListener('load', e => {
-      preview.src = e.target.result
-    })
-    reader.addEventListener('loadend', ()=>{
-      console.log('ayy')
-      console.log(preview.offsetWidth)
-      this._cropSize = Math.min(preview.offsetWidth, preview.offsetHeight)
-      let resizer = this.select('#resizer')
-      resizer.style.width = this._cropSize + 'px'
-      resizer.style.height = this._cropSize + 'px'
-      this._positionX = (preview.offsetWidth - this._cropSize) / 2
-      this._positionY = (preview.offsetHeight - this._cropSize) / 2
-      resizer.style.left = this._positionX + 'px'
-      resizer.style.top = this._positionY + 'px'
-
-      this._mask = this.select('circle')
-      let radius = this._cropSize / 2
-      this._mask.setAttribute('r', radius)
-      this._mask.setAttribute('cx', radius + this._positionX)
-      this._mask.setAttribute('cy', radius + this._positionY)
-
-    })
-    reader.readAsDataURL(e.dataTransfer.files[0])
   }
 
   upload(){
@@ -311,6 +318,7 @@ export default class SociFileDrop extends SociComponent {
   _tempYPos = 0
   _tempCropSize = 0
   _dragging = false
+
   _dragMouseDown(e){
     if(this._dragging) return 0
     this._dragging = true
@@ -327,6 +335,7 @@ export default class SociFileDrop extends SociComponent {
     this._tempCropSize = this._cropSize
     document.body.toggleAttribute('dragging', true)
   }
+
   _dragMouseMove(e){
     this._deltaX = e.clientX - this._mouseDownX
     this._deltaY = e.clientY - this._mouseDownY
@@ -335,19 +344,19 @@ export default class SociFileDrop extends SociComponent {
       case 'drag':
         this._tempYPos = Math.min(
           Math.max(this._positionY + this._deltaY, 0),
-          this.offsetHeight - this._cropSize 
+          this.height - this._cropSize 
         ) 
         this._tempXPos = Math.min(
           Math.max(this._positionX + this._deltaX, 0),
-          this.offsetWidth - this._cropSize 
+          this.width - this._cropSize 
         ) 
         this._setCropCircle(this._tempXPos, this._tempYPos, this._cropSize)
         break
       case 'se':
         this._tempCropSize = Math.min(
           this._cropSize + Math.min(this._deltaY, this._deltaX),
-          this.offsetWidth - this._positionX,
-          this.offsetHeight - this._positionY
+          this.width - this._positionX,
+          this.height - this._positionY
         )
         this._setCropCircle(this._positionX, this._positionY, this._tempCropSize)
         break
@@ -363,7 +372,7 @@ export default class SociFileDrop extends SociComponent {
       case 'ne':
         this._tempCropSize = Math.min(
           this._cropSize - Math.max(this._deltaY, this._deltaX * -1),
-          this.offsetWidth - this._positionX,
+          this.width - this._positionX,
           this._positionY + this._cropSize
         )
         this._tempYPos = this._positionY + this._cropSize - this._tempCropSize
@@ -372,16 +381,15 @@ export default class SociFileDrop extends SociComponent {
       case 'sw':
         this._tempCropSize = Math.min(
           this._cropSize - Math.max(this._deltaY * -1, this._deltaX),
-          this.offsetHeight - this._positionY,
+          this.height - this._positionY,
           this._positionX + this._cropSize
         )
         this._tempXPos = this._positionX + this._cropSize - this._tempCropSize
         this._setCropCircle(this._tempXPos, this._positionY, this._tempCropSize)
         break
     }
-
-    //TODO - Add x axis
   }
+
   _dragMouseUp(e){
     this._positionX = this._tempXPos
     this._positionY = this._tempYPos
@@ -391,29 +399,6 @@ export default class SociFileDrop extends SociComponent {
     document.removeEventListener('mouseup', this._dragMouseUp)
     document.body.toggleAttribute('dragging', false)
     this._dragging = false
-  }
-
-  _seResizeDown(e){
-    this._seResizeMove = this._seResizeMove.bind(this)
-    this._seResizeUp = this._seResizeUp.bind(this)
-    document.addEventListener('mousemove', this._seResizeMove)
-    document.addEventListener('mouseup', this._seResizeUp)
-
-    this._mouseDownX = e.clientX
-    this._mouseDownY = e.clientY
-    document.body.toggleAttribute('dragging', true)
-  }
-  _seResizeMove(e){
-    this._deltaX = e.clientX - this._mouseDownX
-    this._deltaY = e.clientY - this._mouseDownY
-
-    let size = this._cropSize + Math.min(this._deltaY, this._deltaX)
-    this._setCropCircle(this._positionX, this._positionY, size)
-  }
-  _seResizeUp(e){
-    document.removeEventListener('mousemove', this._seResizeMove)
-    document.removeEventListener('mouseup', this._seResizeUp)
-    document.body.toggleAttribute('dragging', false)
   }
 
   _setCropCircle(x, y, size) {
