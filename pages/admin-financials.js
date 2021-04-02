94f0845c372e324364331374fe0970778dee2374
jjcm <j@jjcm.org>
2021-04-02 13:09:14 +1100

    added admin pages

---
+++ pages/admin-financials.js
@@ -0,0 +1,18 @@
+let adminFinancials = {
+  dom: document.currentScript.closest('soci-route'),
+  init: () => {
+    soci.registerPage(adminFinancials)
+  },
+  onActivate: () => {
+    document.title = "Your Financials"
+    adminFinancials.checkFinancials()
+  },
+  onDeactivate: () => {
+  },
+  checkFinancials: async () => {
+    let response = await soci.getData('user/get-financials')
+    adminFinancials.dom.querySelector('.profit h1').innerHTML = `$${Number.parseFloat(response.cash).toPrecision(3)}`
+  },
+}
+
+document.addEventListener('DOMContentLoaded', adminFinancials.init)
