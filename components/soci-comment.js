1d3fde0ed40ce3600ba18b6472876afe4305330f
jjcm <j@jjcm.org>
2021-04-05 12:00:25 +1000

    fixed comment timings being out of sync with the db

--- components/soci-comment.js
+++ components/soci-comment.js
@@ -508,7 +508,7 @@ export default class SociComment extends SociComponent {
       if(res.id){
         //this.select('#submit').success()
         let comment = document.createElement('soci-comment')
-        comment = comment.factory(res.user, 0, 0, res.date, res.id, res.content)
+        comment = comment.factory(res.user, 0, 0, Date.now(), res.id, res.content)
         comment.prependToElement(this)
         setTimeout(() => {
           this._cancelReply()
