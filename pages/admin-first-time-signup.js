fb90f90126221ddb6ffc9d4dd974945e562c3e0e
jjcm <j@jjcm.org>
2021-10-26 19:38:06 -0700

    more signup code

--- pages/admin-first-time-signup.js
+++ pages/admin-first-time-signup.js
@@ -5,10 +5,11 @@ let adminFirstTimeSignup = {
   },
   onActivate: () => {
     document.title = "Nonio - Choose Your Account Type"
+    adminFirstTimeSignup.loadStripe()
   },
   onDeactivate: () => {
   },
-  chooseSupporter: async () => {
+  loadStripe: async () => {
     let stripe = await soci.stripe
     let elements = stripe.elements()
     let currentStyles = getComputedStyle(document.documentElement)
@@ -18,6 +19,7 @@ let adminFirstTimeSignup = {
           iconColor: currentStyles.getPropertyValue('--base-text'),
           color: currentStyles.getPropertyValue('--base-text'),
           fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, "Apple Color Emoji", Arial, sans-serif, "Segoe UI Emoji", "Segoe UI Symbol"',
+          lineHeight: '38px',
           fontSize: '14px',
           ':-webkit-autofill': {
             color: currentStyles.getPropertyValue('--brand-text')
@@ -32,10 +34,26 @@ let adminFirstTimeSignup = {
         }
       }
     })
+    card.on('change', adminFirstTimeSignup.cardError)
     card.mount('#admin-first-time-signup #card-element')
 
-    // TODO: https://stripe.com/docs/billing/subscriptions/fixed-price#add-elements-to-your-page
+    //TODO https://stripe.com/docs/billing/subscriptions/fixed-price#create-subscription
   },
+  cardError: event => {
+    changeLoadingStatePrices(false)
+    let displayError = document.getElementById('card-element-errors')
+    if (event.error) {
+      displayError.textContent = event.error.message
+    } else {
+      displayError.textContent = ''
+    }
+  },
+  chooseSupporter: () => {
+    adminFirstTimeSignup.dom.querySelector('.column.supporter').toggleAttribute('active', true)
+  },
+  subscribe: () => {
+    adminFirstTimeSignup.dom.querySelector('#payment-form').submit()
+  }
 }
 
 document.addEventListener('DOMContentLoaded', adminFirstTimeSignup.init)
