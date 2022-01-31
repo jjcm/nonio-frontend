faa42fb858073f3c262749459305d102872cf570
jjcm <j@jjcm.org>
2022-01-30 18:12:11 -0800

    updated frontend to show stripe link and format cash correctly

--- pages/admin-financials.js
+++ pages/admin-financials.js
@@ -5,17 +5,24 @@ let adminFinancials = {
   },
   onActivate: () => {
     document.title = "Nonio - Your Financials"
+    adminFinancials.dom.querySelector('.header soci-button')?.wait()
     adminFinancials.checkFinancials()
   },
   onDeactivate: () => {
   },
   checkFinancials: async () => {
     let response = await soci.getData('user/get-financials')
-    adminFinancials.dom.querySelector('.profit h1').innerHTML = `$${Number.parseFloat(response.cash || 0).toPrecision(3)}`
-    adminFinancials.dom.querySelector('.stripe h1').innerHTML = `$${Number.parseFloat(response.stripe_wallet_balance || 0).toPrecision(3)}`
-    adminFinancials.dom.querySelectorAll('a').forEach(a => a.href = response.stripe_dashboard_link || "#")
+    adminFinancials.dom.querySelector('.profit h1').innerHTML = adminFinancials.formatCash(response.cash)
+    adminFinancials.dom.querySelector('.stripe h1').innerHTML = adminFinancials.formatCash(response.stripe_wallet_balance || 0)
+    adminFinancials.dom.querySelectorAll('a').forEach(a => a.href = response.stripe_connect_link || "#")
+    adminFinancials.dom.querySelector('.header soci-button')?.removeAttribute('state')
     adminFinancials.dom.querySelector('.subscription .cash').innerHTML = `$${Number.parseFloat(response.subscription_amount || 0)}<span>/mo</span>`
   },
+  formatCash: cash => {
+    cash = Number.parseFloat(cash)
+    let formatter = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'})
+    return formatter.format(cash)
+  }
 }
 
 document.addEventListener('DOMContentLoaded', adminFinancials.init)
